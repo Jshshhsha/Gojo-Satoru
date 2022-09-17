8c1bae050260e9f176b9292e5538573fd6a34796
0968488141 <113769640+0968488141@users.noreply.github.com>
2022-09-17 18:14:35 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,9 +27,9 @@ global.botname = '♛ بسبوس ♛'
 global.footer = ' غوجو بوت ♛'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
-global.sc = 'https://chat.whatsapp.com/CKLmAGJo3iw5AvNM1KZee3'
-global.myweb = 'https://youtube.com/channel/UCPvrnWknQrgCSIeobXMP0Uw'
-global.packname = '𝐏𝐒𝐏𝐎𝐎𝐒𝐀‏⃤‏༼͢🇦🇱 0993944651'
+global.sc = 'https://chat.whatsapp.com/I2t980Jrp3zEjVum6kdsmU'
+global.myweb = 'https://youtu.be/gIW_MsfBXPg'
+global.packname = '𝐏𝐒𝐏𝐎𝐎𝐒𝐀‏⃤‏༼͢🇦🇱 249993944651'
 global.author = '[حقوق الأسطورة]'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
@@ -71,3 +71,4 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 })
+ك
